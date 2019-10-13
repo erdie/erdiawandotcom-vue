4c45d1fdf74641fdaa9b9f64107ac828aade8093
Erdiawan Anna <erdie.one@gmail.com>
2019-10-13 23:17:40 +0700

    fixing tablet view, add pagination for journal

--- src/pages/JournalPosts.vue
+++ src/pages/JournalPosts.vue
@@ -5,7 +5,7 @@
         <section id="blog">
             <div class="container">
                 <div class="blog item">
-                    <div class="d:item__4 t:item__6 m:item__6" v-for="post in posts" :key="post.id">
+                    <div class="d:item__4 t:item__6 m:item__12" v-for="post in posts" :key="post.id">
                         <a v-bind:href="post.link">
                             <div class="blog-card">
                                 <img v-bind:src="post.jetpack_featured_media_url" alt="">
@@ -24,6 +24,10 @@
                 </div>
             </div>
         </section>
+        <Pagination :pagination="pagination"
+                    @prev="--postsData.page; getPosts();"
+                    @next="postsData.page++; getPosts();">
+        </Pagination>
         <Footer></Footer>
     </div>
 </template>
@@ -32,6 +36,7 @@
 import Navigation from '../components/Navigation.vue'
 import NavigationMobile from '../components/Navigation-Mobile.vue'
 import Journal from '../components/Journal.vue'
+import Pagination from '../components/Pagination.vue'
 import Footer from '../components/Footer.vue'
 
 export default {
@@ -39,6 +44,7 @@ export default {
         Navigation,
         NavigationMobile,
         Journal,
+        Pagination,
         Footer
     },
     mounted() {
@@ -51,7 +57,15 @@ export default {
             postsData: {
                 per_page: 9,
                 page: 1
-            }
+            },
+            pagination: {
+                prevPage: '',
+                nextPage: '',
+                totalPages: '',
+                from: '',
+                to: '',
+                total: '',
+            },
         }
     },
     methods: {
@@ -59,10 +73,19 @@ export default {
             axios.get(this.postsUrl, {params: this.postsData})
                 .then((response) => {
                     this.posts = response.data;
+                    this.configPagination(response.headers);
                 })
                 .catch( (error) => {
                     console.log(error);
                 });
+        },
+        configPagination(headers) {
+            this.pagination.total = +headers['x-wp-total'];
+            this.pagination.totalPages = +headers['x-wp-totalpages'];
+            this.pagination.from = this.pagination.total ? ((this.postsData.page - 1) * this.postsData.per_page) + 1 : ' ';
+            this.pagination.to = (this.postsData.page * this.postsData.per_page) > this.pagination.total ? this.pagination.total : this.postsData.page * this.postsData.per_page;
+            this.pagination.prevPage = this.postsData.page > 1 ? this.postsData.page : '';
+            this.pagination.nextPage = this.postsData.page < this.pagination.totalPages ? this.postsData.page + 1 : '';
         }
     },
     metaInfo: {
