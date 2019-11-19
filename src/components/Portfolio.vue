019694467334fdc13bef7d06417a606fda8d9d56
Erdiawan Anna <erdie.one@gmail.com>
2019-11-19 23:21:12 +0700

    update portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -6,7 +6,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/triv-frontend.jpg" class="lazyload" alt="Triv Frontend">
                     <h3>Triv Frontend</h3>
-                    <p>Tech: SASS, HTML5, CSS3, JQUERY <br>
+                    <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
                         Frontend Dev: Anna Erdiawan & Ali Mahfud <br>
                         Demo: <a href="https://triv-frontend.erdiawan.com" target="_blank" rel="noopener">triv-frontend.erdiawan.com</a>
                     </p>
@@ -14,7 +14,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/triv-dashboard.jpg" class="lazyload" alt="Triv Dashboard">
                     <h3>Triv Dashboard</h3>
-                    <p>Tech: SASS, HTML5, CSS3, JQUERY <br>
+                    <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
                         Frontend Dev: Anna Erdiawan & Ali Mahfud <br>
                         Demo: <a href="https://triv-dashboard.erdiawan.com" target="_blank" rel="noopener">triv-dashboard.erdiawan.com</a>
                     </p>
@@ -24,7 +24,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/exp-up.png" class="lazyload" alt="EXP-UP! Academy Esport">
                     <h3>EXP-UP! Academy Esport</h3>
-                    <p>Tech: HTML5, CSS3, SASS, JQUERY<br>
+                    <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://exp-up.id" target="_blank" rel="noopener">exp-up.id</a> & <a href="https://exp-academy.id" target="_blank" rel="noopener">exp-academy.id</a>
                     </p>
@@ -32,7 +32,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/spt.png" class="lazyload" alt="Startup Pilihan Tempo">
                     <h3>Startup Pilihan Tempo</h3>
-                    <p>Tech: HTML5, CSS3, SASS, JQUERY<br>
+                    <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://digitaraya.com/startuppilihantempo/" target="_blank" rel="noopener">digitaraya.com/startuppilihantempo/</a>
                     </p>
@@ -42,7 +42,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/digitaraya.png" class="lazyload" alt="Digitaraya">
                     <h3>Digitaraya</h3>
-                    <p>Tech: HTML5, CSS3, SASS, JQUERY<br>
+                    <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://digitaraya.com" target="_blank" rel="noopener">digitaraya.com</a><br>
                         Demo v1: <a href="https://v1.digitaraya.com" target="_blank" rel="noopener">v1.digitaraya.com</a> (Wordpress)
@@ -51,7 +51,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/digitaraya-tourism.jpg" class="lazyload" alt="Digitaraya Tourism Batch (One Pager)">
                     <h3>Digitaraya Tourism Batch (One Pager)</h3>
-                    <p>Tech: HTML5, CSS3, SASS, JQUERY<br>
+                    <p>Tech Stack: HTML5, CSS3, SASS, JQUERY<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://digitaraya.com/tiketcom-tourismbatch/" target="_blank" rel="noopener">digitaraya.com/tiketcom-tourismbatch/</a>
                     </p>
@@ -61,7 +61,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/simona.jpg" class="lazyload" alt="Simona Ventures">
                     <h3>Simona Ventures</h3>
-                    <p>Tech: SASS, HTML5, CSS3, JQUERY<br>
+                    <p>Tech Stack: SASS, HTML5, CSS3, JQUERY<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://simona.ventures" target="_blank" rel="noopener">simona.ventures</a>
                     </p>
@@ -69,7 +69,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/simona-apacwomen.jpg" class="lazyload" alt="Simona Venture APAC Women ">
                     <h3>Simona Venture APAC Women (One Pager)</h3>
-                    <p>Tech: SASS, HTML5, CSS3, JQUERY <br>
+                    <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://simona.ventures/apacwomen/" target="_blank" rel="noopener">simona.ventures/apacwomen/</a>
                     </p>
@@ -79,7 +79,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/lancong.jpg" class="lazyload" alt="Simona Ventures">
                     <h3>Lancong.id</h3>
-                    <p>Tech: SASS, HTML5, CSS3, JQUERY<br>
+                    <p>Tech Stack: SASS, HTML5, CSS3, JQUERY<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="http://lancong.id" target="_blank" rel="noopener">lancong.id</a>
                     </p>
@@ -87,7 +87,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/kinesys.jpg" class="lazyload" alt="Kinesys Ventures">
                     <h3>Kinesys Ventures</h3>
-                    <p>Tech: SASS, HTML5, CSS3, JQUERY <br>
+                    <p>Tech Stack: SASS, HTML5, CSS3, JQUERY <br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://kinesysgroup.com" target="_blank" rel="noopener">kinesysgroup.com</a>
                     </p>
@@ -97,7 +97,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/mariviu.jpg" class="lazyload" alt="Simona Ventures">
                     <h3>Mariviu</h3>
-                    <p>Tech: WORDPRESS<br>
+                    <p>Tech Stack: WORDPRESS<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://mariviu.com" target="_blank" rel="noopener">mariviu.com</a>
                     </p>
@@ -105,7 +105,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/chromplex.jpg" class="lazyload" alt="Chromplex">
                     <h3>Chromplex</h3>
-                    <p>Tech: WORDPRESS, MATERIAL LITE DESIGN<br>
+                    <p>Tech Stack: WORDPRESS, MATERIAL LITE DESIGN<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="https://chromplex.com" target="_blank" rel="noopener">chromplex.com</a>
                     </p>
@@ -115,7 +115,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/digiskul.jpg" class="lazyload" alt="Simona Ventures">
                     <h3>Digiskul</h3>
-                    <p>Tech: HTML5, CSS3, VUE JS<br>
+                    <p>Tech Stack: HTML5, CSS3, VUE JS<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Backend Dev: Rio Swarawan & Ali Mahfud<br>
                         Demo: <a href="http://digiskul.nuansa.id" target="_blank" rel="noopener">digiskul.nuansa.id</a>
@@ -124,7 +124,7 @@
                 <div class="portfolio-card">
                     <img src="../assets/portfolio/perkim-sumbar.png" class="lazyload" alt="Perkim Sumbar">
                     <h3>Perkim Sumatera Barat</h3>
-                    <p>Tech: HTML5, CSS3, BOOTSTRAP<br>
+                    <p>Tech Stack: HTML5, CSS3, BOOTSTRAP<br>
                         Frontend Dev: Anna Erdiawan<br>
                         Demo: <a href="http://perkim.sumbarprov.go.id" target="_blank" rel="noopener">perkim.sumbarprov.go.id</a>
                     </p>
